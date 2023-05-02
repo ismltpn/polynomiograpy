dbf4c1db79af6c5e2d9f7cdb82be7cb9e866991e
İsmail Tapan <ismail@larsca.com>
2023-05-02 11:57:55 +0300

    version 0.2.0.a1.post1

--- polynomiograpy/__init__.py
+++ polynomiograpy/__init__.py
@@ -3,7 +3,7 @@ from polynomiograpy.common.finite_field import FiniteField
 from polynomiograpy.iterations import compute_screen_for_single_poly
 from polynomiograpy.roots import compute_screen_for_finite_field_poly
 
-__version__ = "0.2.0.a1"
+__version__ = "0.2.0.a1.post1"
 
 __all__ = [
     "compute_screen_for_single_poly",
