042e1f1fce4f839a8657c1dc14cabb94bc7138aa
İsmail Tapan <ismail@larsca.com>
2023-05-09 13:38:55 +0300

    implement multi color for finite field method

--- polynomiograpy/roots/__init__.py
+++ polynomiograpy/roots/__init__.py
@@ -37,3 +37,50 @@ def compute_screen_for_finite_field_poly(
         color_range=color_range,
         channel=channel,
     )
+
+
+def compute_screen_for_finite_field_poly_multi_color(
+    ff: FiniteField,
+    min_degree: int,
+    max_degree: int,
+    width: int,
+    height: int,
+    screen: np.ndarray,
+    screen_buffer: np.ndarray,
+    *,
+    scale_x: float = 0.01,
+    scale_y: float = 0.01,
+    shift_x: float = 0,
+    shift_y: float = 0,
+    color_range: int = 8,
+):
+    roots: list[list[int]] = []
+    for deg in range(min_degree, max_degree + 1):
+        roots_: list[int] = []
+        for poly in ff.generate_polynomials(deg):
+            roots_.extend(poly.roots())
+        roots.append(roots_)
+    return helpers.compute_screen_for_roots_multi_color(
+        roots,
+        width,
+        height,
+        screen,
+        screen_buffer,
+        scale_x=scale_x,
+        scale_y=scale_y,
+        shift_x=shift_x,
+        shift_y=shift_y,
+        color_range=color_range,
+        colors=[
+            np.array([255, 0, 0]),  # red
+            np.array([204, 0, 51]),
+            np.array([153, 0, 102]),
+            np.array([102, 0, 153]),
+            np.array([51, 0, 204]),
+            np.array([0, 0, 255]),  # blue
+            np.array([0, 51, 204]),
+            np.array([0, 102, 153]),
+            np.array([0, 153, 102]),
+            np.array([0, 204, 51]),
+        ],
+    )
--- polynomiograpy/roots/helpers.py
+++ polynomiograpy/roots/helpers.py
@@ -30,3 +30,38 @@ def compute_screen_for_roots(
                 screen_buffer[j, i, channel] = 255
     screen[:, :, channel] = screen_buffer[:, :, channel]
     return np.flipud(screen)
+
+
+def compute_screen_for_roots_multi_color(
+    roots: list[list[int]],
+    width: int,
+    height: int,
+    screen: np.ndarray,
+    screen_buffer: np.ndarray,
+    *,
+    scale_x: float = 1,
+    scale_y: float = 1,
+    shift_x: float = 0,
+    shift_y: float = 0,
+    color_range: int = 8,
+    colors: list[np.ndarray],
+):
+    screen_buffer.fill(0)
+    origin_x = width / 2
+    origin_y = height / 2
+    for deg, roots_ in enumerate(roots):
+        print("Deg:", deg)
+        color = colors[deg]
+        color_resolved = color // color_range
+        for root in roots_:
+            i = int((root.real - shift_x) / scale_x + origin_x)
+            j = int(((root.imag - shift_y) / scale_y + origin_y))
+            if i < 0 or j < 0 or i >= width or j >= height:
+                pass
+            else:
+                screen_buffer[j, i, :] += color_resolved
+                for c in range(3):
+                    if screen_buffer[j, i, c] > 255:
+                        screen_buffer[j, i, c] = 255
+    screen[:, :, :] = screen_buffer[:, :, :]
+    return np.flipud(screen)
