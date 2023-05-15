7e96fe3a137a9cbc2b508aaaafe23462133edac6
İsmail Tapan <ismail@larsca.com>
2023-05-15 13:05:10 +0300

    export multi colored root method

--- polynomiograpy/__init__.py
+++ polynomiograpy/__init__.py
@@ -1,13 +1,17 @@
 from polynomiograpy.common.polynomial import Polynomial
 from polynomiograpy.common.finite_field import FiniteField
 from polynomiograpy.iterations import compute_screen_for_single_poly
-from polynomiograpy.roots import compute_screen_for_finite_field_poly
+from polynomiograpy.roots import (
+    compute_screen_for_finite_field_poly,
+    compute_screen_for_finite_field_poly_multi_color,
+)
 
 __version__ = "0.3.0.a1"
 
 __all__ = [
     "compute_screen_for_single_poly",
     "compute_screen_for_finite_field_poly",
+    "compute_screen_for_finite_field_poly_multi_color",
     "FiniteField",
     "Polynomial",
 ]
